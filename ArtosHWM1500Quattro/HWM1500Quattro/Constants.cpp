3e436549de010477e65a064455e388a041ceb25a
gkocevar1 <kocevar@hotmail.com>
2016-03-02 18:00:40 +0100

    Added libraries and missing files

---
+++ ArtosHWM1500Quattro/HWM1500Quattro/Constants.cpp
@@ -0,0 +1,19 @@
+#include "Constants.h"
+
+char* Constants::PhaseNames[] = 
+{ 
+  "Flushing SF",    //0  - FlushingSFRusco - 1A
+  "Flushing UF",    //1  - FlushingUF1B - 1B
+  "Flushing GAC",   //2  - FlushingGAC - 1C
+  "Filtration",     //3  - Filtration - 2A
+  "Backwash Rusco", //4  - BackwashRusco - 2AA
+  "Flushing UF",    //5  - FlushingUF2B - 2B
+  "Backwash UF1",   //6  - BackwashUF1 - 2C
+  "Backwash UF2",   //7  - BackwashUF2 - 2D 
+  "Backwash UF3",   //8  - BackwashUF3 - 2E
+  "Backwash UF4",   //9  - BackwashUF4 - 2F
+  "Flushing UF",    //10 - FlushingUF2G - 2G 
+  "Desinfection",   //11 - Desinfection
+  "Close"           //12 - Close
+};
+
